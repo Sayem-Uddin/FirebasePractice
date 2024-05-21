55eed1e01540800fe042482c8a1e6f47b5d14739
Sayem-Uddin <uddin2117024@stud.kuet.ac.bd>
2024-05-21 14:11:41 +0600

    user making done

--- src/FirebseProvider/FirebaseProvider.jsx
+++ src/FirebseProvider/FirebaseProvider.jsx
@@ -1,11 +1,32 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+import auth from "../assets/firebase.config";
+import { createUserWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+
 
 
 export const AuthContext = createContext(null)
 
 const FirebaseProvider = ({children}) => {
+    const [User,setUser] = useState(null)
+    console.log(User)
+// Observer
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+        setUser(user)
+        }
+      });
+// creat User
+    const creatUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+// value jas to pass
+    const AllValues ={
+        creatUser ,
+        User,
+        setUser
+    }
     return (
-        <AuthContext.Provider>
+        <AuthContext.Provider value={AllValues}>
             {children}
         </AuthContext.Provider>
     );
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,30 +1,54 @@
-
+import { useContext } from "react";
+import  { AuthContext } from "../../FirebseProvider/FirebaseProvider";
+import { useForm } from "react-hook-form"
 
 const Register = () => {
+  const {
+    register,
+    handleSubmit,
+   
+    formState: { errors },
+  } = useForm()
+
+  const onSubmit = (data) => {
+    const {email,password} = data;
+    creatUser(email,password)
+    .then(result =>{ 
+      })
+  }
+
+
+const {creatUser} = useContext(AuthContext)
+    // console.log(creatUser)
     return (
         <div>
              <div>
               <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100 justify-center items-start mx-auto my-40">
                 <h1 className="text-4xl justify-center mx-auto">Register</h1>
-      <form className="card-body">
+      <form onSubmit={handleSubmit(onSubmit)} className="card-body">
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="email" placeholder="email" className="input input-bordered" required />
+          <input {...register("email", { required: true })} type="email" placeholder="email" className="input input-bordered"  />
+          {errors.email && <span className="text-red-600
+          ">E-mail is required</span>}
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
+          <input {...register("password", { required: true })} type="password" placeholder="password" className="input input-bordered" />
+          {errors.password && <span className="text-red-600
+          ">Password is required</span>}
+          <div className="form-control mt-6">
+         <input className="btn btn-primary" type="Submit" />
+        </div>
           <label className="label">
             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
           </label>
         </div>
-        <div className="form-control mt-6">
        
-        </div>
       </form>
     </div>
         </div>
